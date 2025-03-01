9c6dd75d2ab1121794f251a6cb04b2de57baad64
Rohitprajapati22 <rohitt3891@gmail.com>
2025-03-01 15:11:00 +0530

    marn stack

---
+++ mern-stack/backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const app = express()
+
+const db = require('./config/database')
+db();
+const dotenv = require('dotenv')
+dotenv.config()
+
+
+const cors = require('cors')
+app.use(cors())
+const PORT = process.env.PORT || 9000
+
+app.use(express.urlencoded())
+app.use(express.json())
+
+app.use('/',require('./routes/indexRoute'))
+
+app.listen(PORT,(err) => {
+    if(err){
+        console.log(err)
+        return false
+    }
+    console.log(`Server is Started on port :- ${PORT}`);
+    
+})
